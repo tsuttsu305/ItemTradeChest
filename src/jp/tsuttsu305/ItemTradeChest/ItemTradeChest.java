f103e5c2320bf115cb9ef72d9267365529c0315b
tsuttsu305 <tsuttsu305@daiwa-hotcom.com>
2012-12-13 21:43:24 +0900

    Add Custom Config. Add ja-jp.yml (日本語)

--- src/jp/tsuttsu305/ItemTradeChest/ItemTradeChest.java
+++ src/jp/tsuttsu305/ItemTradeChest/ItemTradeChest.java
@@ -1,6 +1,9 @@
 package jp.tsuttsu305.ItemTradeChest;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import jp.tsuttsu305.config.Config;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,16 +11,26 @@ public class ItemTradeChest extends JavaPlugin {
 	public static ItemTradeChest plugin;
 	Logger logger = Logger.getLogger("Minecraft");
 	public static boolean Lockette = false;
+	private Config conf, msg;
+	
 	@Override
 	public void onEnable(){
 		getServer().getPluginManager().registerEvents(new ShopCreate(), this);
 		getServer().getPluginManager().registerEvents(new PlayerUseShop(this), this);
 		getServer().getPluginManager().registerEvents(new PlayerBlockPlaceEvent(), this);
 		getServer().getPluginManager().registerEvents(new ProtectShop(this), this);
+		
 		if (getServer().getPluginManager().isPluginEnabled("Lockette")){
 			Lockette = true;
 			logger.info("[ItemTradeChest] Hooked Lockette");
 		}
+		
+		conf = new Config("config.yml", this);
+		conf.loadConfig();
+		msg = new Config(conf.getConfig().getString("language") + ".yml", this);
+		msg.loadConfig();
+		logger.log(Level.INFO, "[ItemTradeChest] Load language: " + conf.getConfig().getString("language"));
+		logger.log(Level.INFO,"[ItemTradeChest] " +  msg.getConfig().getString("langChk"));
 	}
 
 }
---
+++ src/jp/tsuttsu305/config/Config.java
@@ -0,0 +1,64 @@
+package jp.tsuttsu305.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import jp.tsuttsu305.ItemTradeChest.ItemTradeChest;
+
+public class Config {
+	private ItemTradeChest plugin;
+	
+	//config関係
+	private FileConfiguration conf;
+	private File confDir;
+	
+	//使用するConfigの名前
+	private  String confName;
+	
+	/**
+	 * 
+	 * @param configName ロードするConfigの名前
+	 * @param plugin JavaPlugin
+	 */
+	public Config(String configName, ItemTradeChest plugin) {
+		this.plugin = plugin;
+		this.confName = configName;
+		//configのパスを取得
+		this.confDir = plugin.getDataFolder();
+	}
+	
+	public void loadConfig(){
+		//Config.ymlへのパスを取得
+		File confPath = new File(confDir, confName);
+		
+		//configが存在しなかった場合はjarからコピー
+		if (!(confPath.exists())){
+			plugin.saveResource(confName, false);
+		}
+		//configを格納
+		conf = YamlConfiguration.loadConfiguration(confPath);
+	}
+	
+	public FileConfiguration getConfig(){
+		if (conf == null){
+			loadConfig();
+		}
+		return conf;
+	}
+	
+	public void saveConfig(){
+		if (conf == null || confDir == null){
+			return;
+		}else{
+			try {
+				getConfig().save(confDir);
+			} catch (IOException e) {
+				plugin.getLogger().log(Level.WARNING, "Cannot save " + confName + " " + e);
+			}
+		}
+	}
+}
